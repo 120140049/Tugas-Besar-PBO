66485fdb5182b401cee802c7d9540731fe463686
= <=>
2022-04-19 23:35:14 +0700

    Ganti nama Hero1 dan Monster1

--- player.py
+++ player.py
@@ -5,10 +5,10 @@ import character
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-class Ulrich(pygame.sprite.Sprite, character.Hero):
+class Alectrona(pygame.sprite.Sprite, character.Hero):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        character.Hero.__init__(self, "Ulrich", 3200, 245)
+        character.Hero.__init__(self, "Alectrona", 3200, 245)
 
         self.image = pygame.Surface([50, 80])
         self.image.fill(WHITE)
@@ -18,10 +18,10 @@ class Ulrich(pygame.sprite.Sprite, character.Hero):
     def serang(self):
         pass
 
-class Demonzilla(pygame.sprite.Sprite, character.Monster):
+class Aposteus(pygame.sprite.Sprite, character.Monster):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        character.Monster.__init__(self, "Demonzilla", 3200, 245)
+        character.Monster.__init__(self, "Aposteus", 3200, 245)
 
         self.image = pygame.Surface([70, 120])
         self.image.fill(WHITE)
