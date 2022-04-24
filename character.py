63c9ea457e2eb1c4504d630c6db1a14c0f1c3a77
Refzlgfr <revanfauzi81@gmail.com>
2022-04-24 11:27:30 +0700

    Update

--- character.py
+++ character.py
@@ -50,3 +50,47 @@ class Monster(Makhluk):
 
     def buff(self):
         pass
+
+class Hero1(Hero):
+    def __init__(self, nama, hp, damage):
+        super().__init__(nama, hp, damage)
+
+    def skill1(self):
+        pass
+
+    def skill2(self):
+        pass
+
+class Hero2(Hero):
+    def __init__(self, nama, hp, damage):
+        super().__init__(nama, hp, damage)
+
+    def skill1(self):
+        pass
+
+    def skill2(self):
+        pass
+
+class Hero3(Hero):
+    def __init__(self, nama, hp, damage):
+        super().__init__(nama, hp, damage)
+
+    def skill1(self):
+        pass
+
+    def skill2(self):
+        pass
+
+class Monster1(Monster):
+    def __init__(self, nama, hp, damage):
+        super().__init__(nama, hp, damage)
+
+    def buff(self):
+        pass
+
+class Monster2(Monster):
+    def __init__(self, nama, hp, damage):
+        super().__init__(nama, hp, damage)
+
+    def buff(self):
+        pass
--- player.py
+++ player.py
@@ -8,7 +8,33 @@ BLACK = (0,0,0)
 class Alectrona(pygame.sprite.Sprite, character.Hero):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        character.Hero.__init__(self, "Alectrona", 3200, 245)
+        character.Hero.__init__(self, "Alectrona", 3000, 200)
+
+        self.image = pygame.Surface([50, 80])
+        self.image.fill(WHITE)
+
+        self.rect = self.image.get_rect()
+
+    def serang(self):
+        pass
+
+class Nipalto(pygame.sprite.Sprite, character.Hero):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        character.Hero.__init__(self, "Nipalto", 2500, 275)
+
+        self.image = pygame.Surface([50, 80])
+        self.image.fill(WHITE)
+
+        self.rect = self.image.get_rect()
+
+    def serang(self):
+        pass
+
+class Salazar(pygame.sprite.Sprite, character.Hero):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        character.Hero.__init__(self, "Salazar", 2000, 350)
 
         self.image = pygame.Surface([50, 80])
         self.image.fill(WHITE)
@@ -21,7 +47,20 @@ class Alectrona(pygame.sprite.Sprite, character.Hero):
 class Aposteus(pygame.sprite.Sprite, character.Monster):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        character.Monster.__init__(self, "Aposteus", 3200, 245)
+        character.Monster.__init__(self, "Aposteus", 5000, 25)
+
+        self.image = pygame.Surface([70, 120])
+        self.image.fill(WHITE)
+
+        self.rect = self.image.get_rect()
+
+    def serang(self):
+        pass
+
+class Fenrir(pygame.sprite.Sprite, character.Monster):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        character.Monster.__init__(self, "Fenrir", 3000, 50)
 
         self.image = pygame.Surface([70, 120])
         self.image.fill(WHITE)
