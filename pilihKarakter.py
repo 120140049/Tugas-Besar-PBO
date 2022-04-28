db819216dcc33ceefb310fd04c1bab1b39198c4e
037-Raditya <muhammad.120140037@student.itera.ac.id>
2022-04-28 15:43:18 +0700

    Update Font dan Ava

--- pilihKarakter.py
+++ pilihKarakter.py
@@ -12,7 +12,7 @@ CWD=os.getcwd()
 pathfile=os.path.join(CWD, "img/pilihKarakter")
 pygame.init()
 layar = pygame.display.set_mode((896,504))
-main_font=pygame.font.SysFont("Times",65)
+main_font=pygame.font.Font("font/A Goblin Appears!.otf",35)
 pos_info=(530,82)
 dimensi_ava=(110,70)
 info_karakter=pygame.image.load(os.path.join(pathfile,'unknownchar.png'))
@@ -20,9 +20,8 @@ klik=None
 
 class TombolPilihan():
     pilihan_karakter=[]
-    def __init__(self, gambar_avatar, pos_x, pos_y, nama_karakter,gambar_info):
+    def __init__(self, pos_x, pos_y, nama_karakter,gambar_info):
         self.nama_karakter=nama_karakter
-        self.gambar_avatar=gambar_avatar
         self.pos_x=pos_x
         self.pos_y=pos_y
         self.gambar_info=gambar_info
@@ -36,11 +35,12 @@ class TombolPilihan():
             381,
             70,
             text=self.nama_karakter,
+            textColour=(255, 255, 255),
             fontSize=65,
             textHAlign=('left'),
-            # font=("Times",65),
-            inactiveColour=(173,217,201),
-            hoverColour=(255, 206, 160),
+            font=pygame.font.Font("font/A Goblin Appears!.otf",35),
+            inactiveColour=(175, 118, 140),
+            hoverColour=(216, 191, 159),
             onClick=lambda: update(self) #muncullah masalah baru
             )
         TombolPilihan.pilihan_karakter.append(tombol)
@@ -50,8 +50,12 @@ def update (karakter):
     klik=True
     info_karakter=karakter.gambar_info
 
-ava=pygame.image.load(os.path.join(pathfile,'test_ava.png'))
-ava=pygame.transform.scale(ava,dimensi_ava)    
+avaAlectrona=pygame.image.load(os.path.join(pathfile,'avaAlectrona.png'))
+avaNipalto=pygame.image.load(os.path.join(pathfile,'avaNipalto.png'))
+avaSalazar=pygame.image.load(os.path.join(pathfile,'avaSalazar.png'))
+avaAposteus=pygame.image.load(os.path.join(pathfile,'avaAposteus.png'))
+avaFenrir=pygame.image.load(os.path.join(pathfile,'avaFenrir.png'))
+   
 background=pygame.image.load(os.path.join(pathfile,'bg_pilihKarakter.jpeg'))
 background=pygame.transform.scale(background,(896,504))    
 
@@ -63,11 +67,11 @@ info_aposteus=pygame.image.load(os.path.join(pathfile,'infoAposteus.png'))
 info_fenrir=pygame.image.load(os.path.join(pathfile,'infoFenrir.png'))
 
 
-tombolAlectrona=TombolPilihan(ava,115,82,"Alectrona",info_alectrona)
-tombolNipalto=TombolPilihan(ava,115,160,"Nipalto",info_nipalto)
-tombolSalazar=TombolPilihan(ava,115,239,"Salazar",info_salazar)
-tombolAposteus=TombolPilihan(ava,115,317,"Aposteus",info_aposteus)
-tombolFenrir=TombolPilihan(ava,115,395,"Fenrir",info_fenrir)
+tombolAlectrona=TombolPilihan(115,82,"Alectrona",info_alectrona)
+tombolNipalto=TombolPilihan(115,160,"Nipalto",info_nipalto)
+tombolSalazar=TombolPilihan(115,239,"Salazar",info_salazar)
+tombolAposteus=TombolPilihan(115,317,"Aposteus",info_aposteus)
+tombolFenrir=TombolPilihan(115,395,"Fenrir",info_fenrir)
 #nanti tambahin lagi atribut warna_respon buat kustomisasi warna respon tiap karakter
 tombol_back=Button(
     layar,
@@ -78,6 +82,7 @@ tombol_back=Button(
 
     text="BACK",
     fontSize=30,
+    font=pygame.font.Font("font/A Goblin Appears!.otf",20),
     textColour='white',
     inactiveColour=(99, 113, 115),
     hoverColour=(217, 17, 17),
@@ -96,15 +101,15 @@ while True:
     layar.blit(info_karakter,(530,82))
     pygame_widgets.update(event)
     #ceritanya ini hero 1
-    layar.blit(ava,(5,82))
+    layar.blit(avaAlectrona,(5,82))
     #ceritanya ini hero 2
-    layar.blit(ava,(5,160))
+    layar.blit(avaNipalto,(5,160))
     #ceritanya ini hero 3
-    layar.blit(ava,(5,239))
+    layar.blit(avaSalazar,(5,239))
     #ceritanya ini hero 4
-    layar.blit(ava,(5,317))
+    layar.blit(avaAposteus,(5,317))
     #ceritanya ini hero 5
-    layar.blit(ava,(5,395))
+    layar.blit(avaFenrir,(5,395))
 
     if klik:
         #ini nanti buat gambar info_karakter
