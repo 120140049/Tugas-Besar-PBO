73dec0a5726540776fb5498aeac02ee77a36eec1
Refzlgfr <revanfauzi81@gmail.com>
2022-05-09 23:19:19 +0700

    fix percabangan spritesheet

--- spritesheet.py
+++ spritesheet.py
@@ -9,7 +9,7 @@ class Spritesheet:
         image = pygame.Surface(rect.size)
         image.blit(self.sheet, (0, 0), rect)
         if colorkey is not None:
-            if colorkey is -1:
+            if colorkey == -1:
                 colorkey = image.get_at((0,0))
             image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
