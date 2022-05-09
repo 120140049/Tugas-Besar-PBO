c0f1742d3bef757b9bb7ba59606af3901843b34a
Refzlgfr <revanfauzi81@gmail.com>
2022-05-09 16:48:19 +0700

    Update UI

--- pilihKarakter.py
+++ pilihKarakter.py
@@ -1,173 +1,193 @@
-# #baru bikin aja
-# #ntar ajala kerjainnya
-import sys
-import os
-import pygame
-import pygame_widgets
-from pygame_widgets.button import Button
-from assetModule import assets_dir, game_chara, game_font
+import pygame, sys, pygame_widgets
+from assetModule import game_env, game_chara, get_font
 
 run = True
-CWD = os.getcwd()
+
+class Button():
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color = base_color
+        self.hovering_color =  hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
+
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
 pygame.init()
-layar = pygame.display.set_mode((896,504))
-main_font = pygame.font.Font(f"{game_font}",35)
-pos_mode = (5,82)
-info_karakter = pygame.image.load(os.path.join(game_chara,'unknownchar.png'))
-Hero = Monster = klik = mode_pilihHero = mode_pilihMonster = karakter = None
-cek_pilih_hero = True
-cek_pilih_monster = False
-teks_karakter = main_font.render(" ? ? ? ? ? ", True, 'white')
-#assets (tutup hero monster, ava, background, gambar info)
 
-#gambar tutup
-mode_pilihHero = pygame.image.load(os.path.join(game_chara,'mode pilih hero.png'))
-mode_pilihMonster = pygame.image.load(os.path.join(game_chara,'mode pilih monster.png'))
-mode = mode_pilihHero
-#gambar ava
-avaAlectrona = pygame.image.load(os.path.join(game_chara,'avaAlectrona.png'))
-avaNipalto = pygame.image.load(os.path.join(game_chara,'avaNipalto.png'))
-avaSalazar = pygame.image.load(os.path.join(game_chara,'avaSalazar.png'))
-avaAposteus = pygame.image.load(os.path.join(game_chara,'avaAposteus.png'))
-avaFenrir = pygame.image.load(os.path.join(game_chara,'avaFenrir.png'))
-#gambar background
-background = pygame.image.load(os.path.join(game_chara,'bg_pilihKarakter.jpeg'))
-background = pygame.transform.scale(background,(896,504))    
-
-#gambar info
-info_alectrona = pygame.image.load(os.path.join(game_chara,'infoAlectrona.png'))
-info_nipalto = pygame.image.load(os.path.join(game_chara,'infoNipalto.png'))
-info_salazar = pygame.image.load(os.path.join(game_chara,'infoSalazar.png'))
-info_aposteus = pygame.image.load(os.path.join(game_chara,'infoAposteus.png'))
-info_fenrir = pygame.image.load(os.path.join(game_chara,'infoFenrir.png'))
-
-#kelas tombol tombol opsi pemain
-class TombolPilihan():
-    # ga kepake
-    # pilihan_karakter=[]
-    def __init__(self, pos_x, pos_y, nama_karakter, gambar_info):
-        self.nama_karakter = nama_karakter
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.gambar_info = gambar_info
-        TombolPilihan.generate(self)
-     #bikin supaya tombol baru bisa dibikin sesuai atribut
-    def generate(self):
-        tombol= Button(
-            layar,
-            self.pos_x,
-            self.pos_y,
-            381,
-            70,
-            text=self.nama_karakter,
-            textColour=(255, 255, 255),
-            fontSize=65,
-            textHAlign=('left'),
-            font=pygame.font.Font(f"{game_font}",35),
-            inactiveColour=(175, 118, 140),
-            hoverColour=(216, 191, 159),
-            onClick=lambda: update(self) 
-            )
-        # ini buat apaan kocak, ga kepake
-        # TombolPilihan.pilihan_karakter.append(tombol)
-
-#update gambar info dan tulisan nama karakter tiap tombol
-def update(objektombol):
-    global info_karakter, klik,teks_karakter, karakter
-    klik = True
-    info_karakter = objektombol.gambar_info
-    teks_karakter = main_font.render(objektombol.nama_karakter,True,'white')
-    # karakter = objektombol.karakter
-
-def nextCommand():
-    global cek_pilih_hero, cek_pilih_monster
-    if Hero == None and Monster == None : 
-        cek_pilih_monster=False
-        pilihHero()
-    elif Hero != None and Monster == None : 
-        cek_pilih_hero=False
-        pilihMonster()
-    elif Hero != None and Monster != None : print("Step Selanjutnya")
-
-def pilihHero():
-    global mode, pos_mode, tombolAlectrona, tombolNipalto, tombolSalazar, \
-            cek_pilih_hero, Hero
-    cek_pilih_hero = True
-    #cetak tombol-tombol ke layar
-    tombolAlectrona = TombolPilihan(115, 82, "Alectrona", info_alectrona)
-    tombolNipalto = TombolPilihan(115,160,"Nipalto",info_nipalto)
-    tombolSalazar = TombolPilihan(115,239,"Salazar",info_salazar)
-    #cetak gambar informasi mode
-    mode = mode_pilihHero
-    pos_mode = (5,317)
-    Hero = karakter
-
-def pilihMonster():
-    global mode, pos_mode, tombolAposteus, tombolFenrir, \
-        cek_pilih_monster, cek_pilih_hero, Monster
-    cek_pilih_hero = False
-    cek_pilih_monster = True
-    #cetak tombol-tombol ke layar
-    tombolAposteus = TombolPilihan(115, 317, "Aposteus", info_aposteus)
-    tombolFenrir = TombolPilihan(115, 395, "Fenrir", info_fenrir)
-    #cetak gambar informasi mode
-    mode = mode_pilihMonster
-    pos_mode = (5, 82)
-    Monster = karakter
-
-tombol_back = Button(layar, 36, 19, 110, 50,
-    text = "Back",
-    font = pygame.font.Font(f"{game_font}",20),
-    textColour = 'white',
-    hoverColour = (128,0,0),
-    inactiveColour = (217, 17, 17),
-    onClick = lambda: print('Tombol back')
-)
-
-tombol_next = Button(layar, 160, 19, 110, 50,
-    text = "Next",
-    font = pygame.font.Font(f"{game_font}",20),
-    textColour = 'white',
-    hoverColour = (124, 145, 153),
-    inactiveColour = (177, 212, 224),
-    onClick = lambda: nextCommand()
-)
-
-def pilihKarakter():
+def pilihhero():
     global run
-    while run:
+    while True:
+        Display = pygame.display.set_mode((896, 504))
+        pygame.display.set_caption("Display Pilih Hero")
+        BG = pygame.image.load(f'{game_env}/bg_pilihTingkatKesulitan.jpeg').convert()
+        BG = pygame.transform.scale(BG,(896, 504))
+        Display.blit(BG, (0, 0))
+
+        Posisi_Mouse = pygame.mouse.get_pos()
+
+        Text_Judul = get_font(40).render("Pilih Hero", True, "red")
+        Judul_Rect = Text_Judul.get_rect(center=(448, 80))
+
+        Tombol_alectrona= Button(image=pygame.image.load(f"{game_env}/button1.png"),
+            pos=(200, 180), text_input="Alectrona", font=get_font(15),
+            base_color="red", hovering_color="White")
+        Tombol_nipalto = Button(image=pygame.image.load(f"{game_env}/button1.png"),
+            pos=(200, 290), text_input="Nipalto", font=get_font(15),
+            base_color="red", hovering_color="White")
+        Tombol_salazar = Button(image=pygame.image.load(f"{game_env}/button1.png"),
+            pos=(200, 400), text_input="Salazar", font=get_font(15),
+            base_color="red", hovering_color="White")
+        Tombol_back = Button(image=pygame.image.load(f"{game_env}/button2.png"),
+            pos=(100, 50), text_input="Back", font=get_font(15), base_color="red",
+            hovering_color="White")
+
+        Display.blit(Text_Judul, Judul_Rect)
+
+        image_base = pygame.image.load(f'{game_chara}/unknownchar.png')
+        #image_base = image_info.get_rect(center = (530,82))
+
+
+        #pos_info=(530,82)
+
+        for button in [Tombol_alectrona, Tombol_nipalto, Tombol_salazar,Tombol_back]:
+            button.changeColor(Posisi_Mouse)
+            button.update(Display)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
                 pygame.quit()
                 sys.exit()
-        events = pygame.event.get()
-        pilihHero()
-        layar.fill("white")
-        layar.blit(background,(0,0))
-        layar.blit(info_karakter,(530,82))
-        pygame_widgets.update(events)
-        if cek_pilih_hero:
-            #ini ava Alectrona
-            layar.blit(avaAlectrona,(5,82))
-            #ini ava Nipalto
-            layar.blit(avaNipalto,(5,160))
-            #ini ava Salazar
-            layar.blit(avaSalazar,(5,239))
-        if cek_pilih_monster:
-            #ini ava Aposteus
-            layar.blit(avaAposteus,(5,317))
-            #ini ava Fenrir
-            layar.blit(avaFenrir,(5,395))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Tombol_alectrona.checkForInput(Posisi_Mouse):
+                    state = 'Alectrona'
+                    return state
+                    run = False
+                if Tombol_nipalto.checkForInput(Posisi_Mouse):
+                    state = 'Nipalto'
+                    return state
+                    run = False
+                if Tombol_salazar.checkForInput(Posisi_Mouse):
+                    state = 'Salazar'
+                    return state
+                    run = False
+                if Tombol_back.checkForInput(Posisi_Mouse):
+                    state = 'Back'
+                    return state
+                    run = False
 
-        if klik:
-            #ini nanti buat gambar info_karakter
-            layar.blit(info_karakter,(530,82))
 
-        #ini buat nama karakter dibawah foto info
-        layar.blit(teks_karakter,(510,415))
-        #
-        layar.blit(mode,pos_mode)
-        #taro paling bawah, ini buat update semua perubahan
+        if Tombol_alectrona.checkForInput(Posisi_Mouse):
+            image_base=pygame.image.load(f'{game_chara}/infoAlectrona.png')
+            Display.blit(image_base,(450,130))
+        if Tombol_nipalto.checkForInput(Posisi_Mouse):
+            image_base=pygame.image.load(f'{game_chara}/infoNipalto.png')
+            Display.blit(image_base,(450,130))
+        if Tombol_salazar.checkForInput(Posisi_Mouse):
+            image_base=pygame.image.load(f'{game_chara}/infoSalazar.png')
+            Display.blit(image_base,(450,130))
+
+        Display.blit(image_base,(450,130))
+        pygame_widgets.update(event)
+
         pygame.display.update()
 
+def pilihmonster():
+    global run
+    while True:
+        events = pygame.event.get()
+        Display = pygame.display.set_mode((896, 504))
+        pygame.display.set_caption("Display Pilih Monster")
+        BG = pygame.image.load(f"{game_env}/bg_pilihTingkatKesulitan.jpeg").convert()
+        BG = pygame.transform.scale(BG,(896, 504))
+        Display.blit(BG, (0, 0))
+
+        Posisi_Mouse = pygame.mouse.get_pos()
+
+        Text_Judul = get_font(40).render("Pilih Monster", True, "red")
+        Judul_Rect = Text_Judul.get_rect(center=(448, 80))
+
+        Tombol_aposteus= Button(image=pygame.image.load(f"{game_env}/button1.png"),
+            pos=(200, 180), text_input="aposteus", font=get_font(15),
+            base_color="red", hovering_color="White")
+        Tombol_fenrir = Button(image=pygame.image.load(f"{game_env}/button1.png"),
+            pos=(200, 290), text_input="fenrir", font=get_font(15),
+            base_color="red", hovering_color="White")
+        Tombol_back = Button(image=pygame.image.load(f"{game_env}/button2.png"),
+            pos=(100, 50), text_input="Back", font=get_font(15), base_color="red",
+            hovering_color="White")
+
+        Display.blit(Text_Judul, Judul_Rect)
+        image_base = pygame.image.load(f'{game_chara}/unknownchar.png')
+        #image_base = image_info.get_rect(center = (530,82))
+
+
+        #pos_info=(530,82)
+
+        for button in [Tombol_aposteus, Tombol_fenrir,Tombol_back]:
+            button.changeColor(Posisi_Mouse)
+            button.update(Display)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Tombol_aposteus.checkForInput(Posisi_Mouse):
+                    state = 'Aposteus'
+                    return state
+                    run = False
+                if Tombol_fenrir.checkForInput(Posisi_Mouse):
+                    state = 'Fenrir'
+                    return state
+                    run = False
+                if Tombol_back.checkForInput(Posisi_Mouse):
+                    state = 'Back'
+                    return state
+                    run = False
+
+        if Tombol_aposteus.checkForInput(Posisi_Mouse):
+            image_base=pygame.image.load(f'{game_chara}/infoAposteus.png')
+            Display.blit(image_base,(450,130))
+        if Tombol_fenrir.checkForInput(Posisi_Mouse):
+            image_base=pygame.image.load(f'{game_chara}/infoFenrir.png')
+            Display.blit(image_base,(450,130))
+
+        Display.blit(image_base,(450,130))
+        pygame_widgets.update(events)
+
+        pygame.display.update()
+
+def main():
+    x = pilihhero()
+    if x == 'Back':
+        sys.exit()
+    y = pilihmonster()
+    if y == 'Back':
+        main()
+
+main()
